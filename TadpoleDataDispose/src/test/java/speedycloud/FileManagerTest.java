47eed852402214d04259c645622ac29e99d0dfb6
a814511580 <1306820198@qq.com>
2018-02-08 14:44:36 +0800

    qqq

--- TadpoleDataDispose/src/test/java/speedycloud/FileManagerTest.java
+++ TadpoleDataDispose/src/test/java/speedycloud/FileManagerTest.java
@@ -8,7 +8,7 @@ public class FileManagerTest {
 				"http://cos.speedycloud.org",
 				"16C0763318C3F68EF958183672F70465",
 				"4ca971687f254bfca37d9e5584da9499c9f3be4c6f77fbbb47d5030102e44e2c");
-		fileManager.createdFolder("tadpole", "F:\\Test--III\\test25\\YellowThird\\app\\src");
+		fileManager.createdFolder("tadpole", "F:\\xiaomi\\app数据点\\data");
 	}  
 	
 	public static void main(String[] args)
