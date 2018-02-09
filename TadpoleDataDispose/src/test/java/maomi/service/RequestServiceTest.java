f95a1367c1c679ec20b84f25cb7b1d7350c84ccb
a814511580 <1306820198@qq.com>
2018-02-09 23:06:34 +0800

    sds

---
+++ TadpoleDataDispose/src/test/java/maomi/service/RequestServiceTest.java
@@ -0,0 +1,17 @@
+package maomi.service;
+
+public class RequestServiceTest {
+
+	
+	
+	public static void main(String[] args) throws Exception
+	{
+		//new RequestService().getRecommendCommunity(); 
+		//new RequestService().getAssignCircle("7");
+		new RequestService().getListCommentsData();
+		//getListCommentsData() 
+	} 
+	
+	
+	
+}
